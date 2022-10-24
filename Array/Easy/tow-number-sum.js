2b82bf8ff6a75d5a3a058df016f8b54fd7059e2d
Anas <alnabale2016@gmail.com>
2022-10-24 14:45:40 +0300

    array e-1

--- Array/Easy/tow-number-sum.js
+++ Array/Easy/tow-number-sum.js
@@ -26,9 +26,58 @@ implications of this approach?
 Hint 2
 Realize that for every number X in the input array, you are essentially trying to find a corresponding number Y such
 that X + Y = targetSum. With two variables in this equation known to you, it shouldn't be hard to solve for Y.
-Hint 3
 
+Hint 3
 Try storing every number in a hash table, solving the equation mentioned in Hint #2 for every number, and checking
 if the Y that you find is stored in the hash table. What are the time and space implications of this approach?
 Optimal Space & Time Complexity
 */
+
+array = [4, 6, 1, -3],
+targetSum = 3
+
+const solution1 = () => {
+    for (let i in array) {
+        for (let j in array) {
+            if (array[i] + array[j] === targetSum && i !== j)
+                return [array[i], array[j]]
+        }
+    }
+    return []
+}
+
+const solution2=(array,targetSum)=>{
+    const numbersObj={}
+    let y;
+    for (const x of array) {
+        y=targetSum-x
+        if(numbersObj[y]){
+            return[x,y]
+        }
+        else{
+            numbersObj[x]=true
+        }
+    }
+    return []
+}
+
+const solution3=(array,targetSum)=>{
+    let leftAndRightSum=0;
+    array.sort((first,second)=>{
+        return first-second
+    })
+    let leftPointer=0
+    let rightPointer=array.length-1
+
+    while (leftPointer < rightPointer) {
+        leftAndRightSum=array[leftPointer]+array[rightPointer]
+        if(leftAndRightSum===targetSum){
+            return [array[leftPointer],array[rightPointer]]
+        }
+        leftAndRightSum < targetSum? leftPointer++:rightPointer--;
+    }
+    return []
+
+}
+
+
